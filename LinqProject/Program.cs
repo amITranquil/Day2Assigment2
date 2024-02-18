15324a19385ef14d5e9cfc7860343a36a2f9fd84
amITranquil <tranquil.95@gmail.com>
2024-02-18 19:55:47 +0300

    2. ödev sonu

---
+++ LinqProject/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Category> categories = new List<Category>
+            {
+                new Category{CategoryId=1,CategoryName="PC"},
+                new Category{CategoryId=2,CategoryName="Phone"}
+            };
+            List<Product> products = new List<Product>
+            {
+                new Product{ProductId=1, CategoryId=1,ProductName="Casper",ProductDescription="16GB Ram",UnitPrice=1200,UnitsInStock=5},
+                new Product{ProductId=2,CategoryId=2,ProductName="Poco",ProductDescription="8GB Ram",UnitPrice=1400,UnitsInStock=15},
+                new Product{ProductId=3,CategoryId=1,ProductName="HP",ProductDescription="24GB Ram",UnitPrice=1600,UnitsInStock=25},
+                new Product{ProductId=4,CategoryId=1,ProductName="Dell",ProductDescription="32GB Ram",UnitPrice=1800,UnitsInStock=35},
+                new Product{ProductId=5,CategoryId=2,ProductName="Apple",ProductDescription="64GB Ram",UnitPrice=2000,UnitsInStock=45},
+            };
+
+            Console.WriteLine("Without Linq---------------");
+
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 1200 && product.UnitsInStock > 16)
+                {
+                    Console.WriteLine(product.ProductName);
+                }
+
+            }
+            Console.WriteLine("Linq---------------");
+
+            var result = products.Where(p => p.UnitPrice > 1200 && p.UnitsInStock > 16);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            GetProducts(products);
+
+            Console.ReadLine();
+        }
+
+        static List<Product> GetProducts(List<Product> products)
+        {
+            List<Product> filteredProducts = new List<Product>();
+
+            foreach (var product in products)
+            {
+                if (product.UnitPrice > 1200 && product.UnitsInStock > 16)
+                {
+                    filteredProducts.Add(product);
+                }
+            }
+            return filteredProducts;
+        }
+
+        static List<Product> GetProductsLinq(List<Product> products)
+        {
+            return products.Where(p => p.UnitPrice > 1200 && p.UnitsInStock > 16).ToList();
+        }
+
+    }
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
---
+++ ThreadingDemo/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ThreadingDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess1 = new Syncfusion.WinForms.Controls.SfButton();
+            this.btnProcess2 = new Syncfusion.WinForms.Controls.SfButton();
+            this.SuspendLayout();
+            // 
+            // btnProcess1
+            // 
+            this.btnProcess1.AccessibleName = "Button";
+            this.btnProcess1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F);
+            this.btnProcess1.Location = new System.Drawing.Point(58, 52);
+            this.btnProcess1.Name = "btnProcess1";
+            this.btnProcess1.Size = new System.Drawing.Size(159, 58);
+            this.btnProcess1.TabIndex = 0;
+            this.btnProcess1.Text = "Process1";
+            this.btnProcess1.Click += new System.EventHandler(this.btnProcess1_Click);
+            // 
+            // btnProcess2
+            // 
+            this.btnProcess2.AccessibleName = "Button";
+            this.btnProcess2.Font = new System.Drawing.Font("Segoe UI Semibold", 9F);
+            this.btnProcess2.Location = new System.Drawing.Point(260, 52);
+            this.btnProcess2.Name = "btnProcess2";
+            this.btnProcess2.Size = new System.Drawing.Size(163, 47);
+            this.btnProcess2.TabIndex = 1;
+            this.btnProcess2.Text = "Process2";
+            this.btnProcess2.Click += new System.EventHandler(this.btnProcess2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnProcess2);
+            this.Controls.Add(this.btnProcess1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Syncfusion.WinForms.Controls.SfButton btnProcess1;
+        private Syncfusion.WinForms.Controls.SfButton btnProcess2;
+    }
+}
+
